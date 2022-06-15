18889cb5f1909593c6067029b7e4c6177e6aa861
Nikita <105992775+GiShame@users.noreply.github.com>
2022-06-15 15:34:42 +0300

    Close/open eventListener

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 'use strict';
 
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal')
+
+const openModalWindow = function() {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+const closeModalWindow = function() {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+btnsOpenModal[i].addEventListener('click', openModalWindow)
+
+btnCloseModal.addEventListener('click', closeModalWindow)
+overlay.addEventListener('click', closeModalWindow)
