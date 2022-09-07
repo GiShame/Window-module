9960ebfc97ef029eddfdd6a5bbf02225fba605a1
Nikita <105992775+GiShame@users.noreply.github.com>
2022-09-07 13:17:39 +0300

    Completed Modal Window

--- script.js
+++ script.js
@@ -1,59 +1,27 @@
-'use strict'
+'use strict';
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1
-let score = 20
-let highscore = 0
-const displayMessage = function (message) {
-  document.querySelector('.message').textContent = message
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal')
+
+const openModalWindow = function() {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+const closeModalWindow = function() {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
 }
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value)
+for (let i = 0; i < btnsOpenModal.length; i++)
+btnsOpenModal[i].addEventListener('click', openModalWindow)
 
-  //   When there is no input
-  if (!guess) {
-    if (score > 1) {
-      displayMessage('STUPID!')
-      score--
-      document.querySelector('.score').textContent = score
-    } else {
-      displayMessage('You lost!')
-      document.querySelector('.score').textContent = 0
+btnCloseModal.addEventListener('click', closeModalWindow)
+overlay.addEventListener('click', closeModalWindow)
+
+document.addEventListener('keydown', function(btn) {
+    if (btn.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModalWindow();
     }
-
-    //  When player wins
-  } else if (guess === secretNumber) {
-    displayMessage('YOU are the BEST!')
-    document.querySelector('.number').textContent = secretNumber
-    document.querySelector('body').style.backgroundColor = '#00d200'
-    document.querySelector('.number').style.width = '30rem'
-    document.querySelector('.number').style.fontSize = '10rem'
-
-    if (score > highscore) {
-      highscore = score
-      document.querySelector('.highscore').textContent = highscore
-    }
-  } else if (guess !== secretNumber) {
-    if (score > 1) {
-      displayMessage(guess > secretNumber ? 'Too high!' : 'Too low')
-      score--
-      document.querySelector('.score').textContent = score
-    } else {
-      displayMessage('You lost!')
-      document.querySelector('.score').textContent = 0
-      document.querySelector('body').style.backgroundColor = 'red'
-    }
-  }
-})
-
-document.querySelector('.again').addEventListener('click', function () {
-  secretNumber = Math.trunc(Math.random() * 20) + 1
-  score = 20
-  document.querySelector('.score').textContent = score
-  displayMessage('Start guessing...')
-  document.querySelector('body').style.backgroundColor = '#222'
-  document.querySelector('.number').style.width = '15rem'
-  document.querySelector('.number').style.fontSize = '6rem'
-  document.querySelector('.number').textContent = '?'
-  document.querySelector('.guess').value = ''
 })
